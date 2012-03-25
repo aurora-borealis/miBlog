86079cbd8ebd1a1d2e0ed413b48972e93a580742
Emin İnanç Ünlü <einancunlu@gmail.com>
2012-03-25 19:02:05 +0300

    Duzenleme

--- application/views/login.php
+++ application/views/login.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Le styles -->
-    <?php foreach ($link as $i): ?>
-    <?php echo link_tag($i); ?>
+    <?php foreach ($miBlogConfig['css'] as $css): ?>
+    <?php echo link_tag($css); ?>
     <?php endforeach; ?>
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
