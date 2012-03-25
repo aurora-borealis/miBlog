1bc9a8af658a33b713ead47c4af0a89c60b28611
Emin İnanç Ünlü <einancunlu@gmail.com>
2012-03-25 17:43:20 +0300

    miBlog kurulum sayfası düzenlendi

--- application/views/blogInstall_index.php
+++ application/views/blogInstall_index.php
@@ -20,6 +20,10 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+
+    <script type="text/javascript">
+    	
+    </script>
   </head>
 
   <body>
@@ -31,77 +35,80 @@
           <div class="seperator"></div>
           <div class="gray2">Kurulum Sayfası</div>
         </div>
-      </div>
-      <div class="row">
-        <div class="well span6 offset3">	          
-          <form class="form-horizontal">
-            <fieldset>
-              <legend class="gray2 formTitleFont bold">Kullanıcı Bilgileri</legend>
-              <div class="control-group">
-                <label class="control-label bold gray4" for="input01">Kullanıcı Adı:</label>
-                <div class="controls">
-                  <input type="text" class="input-xlarge span3" id="input01">
-                  <p class="help-block gray2">Bloğunuza giriş yaparken kullanacağınız kullanıcı adı.</p>
-                </div>
-              </div>
-
-              <div class="control-group">
-                <label class="control-label bold gray4" for="input01">Şifre:</label>
-                <div class="controls">
-                  <input type="text" class="input-xlarge span3" id="input01">
-                  <p class="help-block gray2">En az 5 karakter, Türkçe karakterler yok.</p>
-                </div>
-              </div>
-            </fieldset>
-
-            <fieldset>
-              <legend class="gray2 formTitleFont bold">Blog Bilgileri</legend>
-              <p class="fieldsetInfo gray3">İstemiyorsanız boş bırakın, sonra da ekleyip değiştirebilirsiniz.</p>
-
-              <div class="control-group">
-                <label class="control-label bold gray4" for="input01">Blog Başlığı:</label>
-                <div class="controls">
-                  <input type="text" class="input-xlarge span3" id="input01">
-                  <p class="help-block gray2">En üst kısmında görünecek başlık.</p>
-                </div>
-              </div>
-
-              <div class="control-group">
-                <label class="control-label bold gray4" for="input01">Kısa Not:</label>
-                <div class="controls">
-                  <textarea class="input-xlarge span3" id="textarea" rows="4"></textarea>
-                  <p class="help-block gray2">Blog başlığının yanında gözükecek kısa yazı. <br> En fazla 160 karakter olmalı.</p>
-                </div>
-              </div>
-            </fieldset>
-
-            <div class="form-actions form-actions-paddingCorrect">
-              <button type="submit" class="btn btn-info pull-right">
-                Devam
-                <i class="icon-chevron-right icon-white"></i>  
-              </button>
-            </div>
-          </form>
-        </div> <!-- end .formsContainer -->
       </div> <!-- end .row -->
+
+      <div class="row">
+      	<form class="form-horizontal whiteTextShadow5">
+      	  <fieldset>
+      	  	<span class="wellOuterTitle offset2">KULLANICI BİLGİLERİ</span>
+      	  	<div class="wellOpposite wellOpposite-small span8 offset2">
+      	  		<div class="mainInfos">
+      	  			<div class="control-group">
+      	  			  <label class="control-label bold gray3" for="input01">Kullanıcı Adı:</label>
+      	  			  <div class="controls">
+      	  			    <input type="text" class="span3" placeholder="omerfaruk" id="input01">
+      	  			    <p class="help-block gray2">Bloğunuza giriş yaparken kullanacağınız ad, esas adınız ve soyadınız olmak zorunda değil.</p>
+      	  			  </div>
+      	  			</div> 
+      	  			
+      	  			<div class="control-group">
+      	  			  <label class="control-label bold gray3" for="input01">Şifre:</label>
+      	  			  <div class="controls">
+      	  			    <input type="password" class="span3" id="input01">
+      	  			    <p class="help-block gray2">En az 5 basamaklı olmalı; İngilizce harfler, sayı ve noktalama işaretleri kullanılabilir.</p>
+      	  			  </div>
+      	  			</div> 
+      	  		</div> <!-- end .mainInfos -->
+      	  	</div>  <!-- end .wellOpposite -->
+      	  </fieldset>
+      	  
+      	  <fieldset>
+      	  	<span class="wellOuterTitle offset2">BLOG BİLGİLERİ
+      	  		<div class="wellOuterTitleHelper">
+      	  			<div class="seperator"></div>
+      	  			<span>Opsiyonel alan, daha sonra da doldurabilirsiniz</span>
+      	  		</div>
+      	  	</span>
+      	  	<div class="wellOpposite wellOpposite-small span8 offset2">
+      	  		<div class="mainInfos">
+      	  			<div class="control-group">
+      	  			  <label class="control-label bold gray3" for="input01">Blog Başlığı:</label>
+      	  			  <div class="controls">
+      	  			    <input type="text" class="span3" id="input01">
+      	  			    <p class="help-block gray2">En üst kısmında gözükecek başlık.</p>
+      	  			  </div>
+      	  			</div> 
+      	  			
+      	  			<div class="control-group">
+      	  			  <label class="control-label bold gray3" for="input01">Blog Hakkında:</label>
+      	  			  <div class="controls">
+      	  			    <textarea class="input-xlarge span3" id="textarea" rows="4"></textarea>
+      	  			    <p class="help-block gray2">Blog başlığının yanında gözükecek kısa not. En fazla 160 karakter olabilir.</p>
+      	  			  </div>
+      	  			</div> 
+      	  		</div> <!-- end .mainInfos -->
+      	  	</div>  <!-- end .wellOpposite -->
+      	  </fieldset>
+      	  
+      	  <div class="well well-small span8 offset2">
+            <a href="aboutInstall.html" type="submit" class="btn btn-success pull-right">
+              Devam
+              <i class="icon-chevron-right icon-white"></i> 
+            </a>
+          </div> <!-- end .well -->
+      	</form>
+        
+        <div class="clearBoth"></div>
+      </div> <!-- end .row -->
+
+      <div class="emptyFooter"></div>
     </div> <!-- end .container -->
 
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
-    <script src="js/bootstrap-alert.js"></script>
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"></script>
 
   </body>
 </html>
