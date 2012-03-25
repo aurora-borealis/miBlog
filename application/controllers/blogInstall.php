9634a5d2400bfb8491f0d43addcd0b66ad1b6a98
Burak Yildiz <yildizburak@msn.com>
2012-03-25 21:08:57 +0300

    model sistemi eklendi

--- application/controllers/blogInstall.php
+++ application/controllers/blogInstall.php
@@ -1,13 +1,20 @@
 <?php
 
 class BlogInstall extends mB_Controller {
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Admin_model');
+  }
 
   public function index() {
+    if ($this->Admin_model->isInstalled())
+      redirect('home');
+    
     $this->load->view('blogInstall_index', $this->data);
   }
   
   public function about() {
     $data = array();
-    $this->load->view('blogInstall_about', $data);
+    $this->load->view('blogInstall_about', $this->data);
   }
 }
